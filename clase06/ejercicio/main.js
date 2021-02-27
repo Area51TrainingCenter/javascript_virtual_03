6ba63649e1314bbecd32be440babe6c0d11a42df
unknown <jtorriccelli1986@gmail.com>
2021-02-27 14:03:04 -0500

    update

--- clase06/ejercicio/main.js
+++ clase06/ejercicio/main.js
@@ -1,2 +1,52 @@
 //URL DE CONSUMO DE PRODUCTOS
 // https://sminnova.com/productos.json
+
+let obj_servicios=new ProductosCarrito();
+
+obj_servicios.obtenerProductos()
+
+// true si hay productos 
+// false si no hay productos
+console.log(obj_servicios.validarProductosCarrito());
+
+if(obj_servicios.validarProductosCarrito()){
+    // si hay productos
+    actualizarMiniCarrito();
+}
+else{
+    actualizarMiniCarrito(0)
+    // no hay productos
+}
+
+
+function agregarcarrito(indice){
+    obj_servicios.registarEnCarrito(indice);
+    // necesito obtener los productos registrados en el carrito
+    actualizarMiniCarrito();
+  
+}
+
+function actualizarMiniCarrito(parametro=1){
+    if(parametro==0){
+        document.getElementById("productos_carrito").innerHTML="<span>No hay productos seleccionados</span>";
+    }
+    else{
+        let datos=obj_servicios.obtenerProductosCarrito();
+        let temp="";
+                for(let i=0;i<datos.length;i++){
+                    
+                    temp=temp+`
+                    <article class="producto-carrito">
+                        <div class="forma">
+                            <img src="${datos[i].foto}" alt="">
+                            <h2>${datos[i].nombre}</h2>
+                            </div>
+                            
+                    </article>
+                    `;
+    
+                }
+        document.getElementById("productos_carrito").innerHTML=temp;
+    }
+    
+}
--- clase06/ejercicio/servicios.js
+++ clase06/ejercicio/servicios.js
@@ -1,3 +1,50 @@
-class Servicios{
-    
+class ProductosCarrito{
+    constructor(){
+        this.listadoProductos;
+        this.productosCarrito=[];
+    }
+    obtenerProductos(){
+        let url="productos.json";
+         fetch(url)
+        .then((data)=>{return data.json()})
+        .then((data)=>{
+            this.listadoProductos=data;
+            
+            let temp="";
+            for(let i=0;i<data.length;i++){
+                
+                temp=temp+`
+                <article class="producto">
+                    <img src="${data[i].foto}">
+                    <h2>${data[i].nombre}</h2>
+                    <button onclick="agregarcarrito(${i})">Agregar al carrito</button>
+                </article>
+                `;
+
+            }
+            document.getElementById("productos").innerHTML=temp;
+        })
+    }
+    registarEnCarrito(indice){
+        //console.log(this.listadoProductos[indice]);
+       this.productosCarrito.push(this.listadoProductos[indice]);
+       localStorage.productosCarrito=JSON.stringify(this.productosCarrito);
+     
+    }
+    obtenerProductosCarrito(){
+        return this.productosCarrito;
+    }
+    validarProductosCarrito(){
+        let estado=false;
+        if(localStorage.productosCarrito){
+            //let datos=JSON.parse(localStorage.productosCarrito)
+            this.productosCarrito=JSON.parse(localStorage.productosCarrito)
+            estado=true;
+        }
+        else{
+            estado=false;
+            //console.log("no hay datos")
+        }
+        return estado;
+    }
 }
